80f5b83509cb912fe24ec9786ff9fd470fc63527
neochin <neochin@163.com>
2024-08-26 13:03:05 +0800

    add qt charts modules install

--- install-aqt.sh
+++ install-aqt.sh
@@ -1,16 +1,24 @@
-  aqt install-qt mac desktop 5.15.2 --outputdir ~/Qt
-  aqt install-qt mac android 5.15.2 --outputdir ~/Qt
-  aqt install-qt mac ios 5.15.2 --outputdir ~/Qt
+# doc: https://aqtinstall.readthedocs.io/_/downloads/en/latest/pdf/
 
-  aqt install-src mac 5.15.2 --outputdir ~/Qt
-  aqt install-doc mac 5.15.2 --outputdir ~/Qt
+aqt install-qt mac desktop 5.15.2 --outputdir ~/Qt
+aqt install-qt mac android 5.15.2 --outputdir ~/Qt
+aqt install-qt mac ios 5.15.2 --outputdir ~/Qt
 
-  aqt install-tool mac desktop tools_qtdesignstudio --outputdir ~/Qt
-  aqt install-tool mac desktop tools_cmake --outputdir ~/Qt
-  aqt install-tool mac desktop sdktool --outputdir ~/Qt
-  aqt install-tool mac desktop tools_ifw --outputdir ~/Qt
-  aqt install-tool mac desktop tools_ninja --outputdir ~/Qt
+# aqt list-qt mac ios --modules 5.15.2 ios
+# aqt install-qt mac desktop 5.15.2 clang_64 -m qtcharts qtquick3d qtwebengine qtwebglplugin --outputdir ~/Qt
+aqt install-qt mac desktop 5.15.2 -m qtcharts --outputdir ~/Qt
+aqt install-qt mac android 5.15.2 -m qtcharts --outputdir ~/Qt
+aqt install-qt mac ios 5.15.2 -m qtcharts --outputdir ~/Qt
 
-  aqt install-example mac 5.15.2 --outputdir ~/Qt
+aqt install-src mac 5.15.2 --outputdir ~/Qt
+aqt install-doc mac 5.15.2 --outputdir ~/Qt
 
-  # aqt install-tool mac desktop tools_qtcreator --outputdir ~/Qt
+aqt install-tool mac desktop tools_qtdesignstudio --outputdir ~/Qt
+aqt install-tool mac desktop tools_cmake --outputdir ~/Qt
+aqt install-tool mac desktop sdktool --outputdir ~/Qt
+aqt install-tool mac desktop tools_ifw --outputdir ~/Qt
+aqt install-tool mac desktop tools_ninja --outputdir ~/Qt
+
+aqt install-example mac 5.15.2 --outputdir ~/Qt
+
+# aqt install-tool mac desktop tools_qtcreator --outputdir ~/Qt
